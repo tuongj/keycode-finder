32e8e67ddb84e03ddf72bd21542cc6e46f1285ca
tuongj <tuongj@gmail.com>
2018-11-25 23:04:27 -0600

    third commit

--- js/script.js
+++ js/script.js
@@ -5,10 +5,13 @@ let key = {
   charEvent: {},
   init: function() {
     this.cacheDom();
+    this.startPrompt();
     this.bindEvents();
     this.clearCell(false);
   },
   cacheDom: function() {
+    this.toggle = document.getElementsByClassName('toggle');
+    
     this.keyTable = document.getElementById('keyTable');
     this.keycode = this.keyTable.querySelector('.keycode');
     this.charcode = this.keyTable.querySelector('.charcode');
@@ -17,8 +20,6 @@ let key = {
     this.ctrl = this.keyTable.querySelector('.ctrl');
     this.alt = this.keyTable.querySelector('.alt');
     this.meta = this.keyTable.querySelector('.meta');
-    
-    this.toggle = document.getElementsByClassName('toggle');
 
   },
   clearCell: function(del = true) {
@@ -29,8 +30,9 @@ let key = {
     for (let i = 1; i < tableColumns; i++) {
       // loop through rows
       for (let x = 1; x < tableRows; x++) {
+        // replace all cells with dashes
         if (del) {
-         this.keyTable.rows[i].deleteCell(x); 
+         this.keyTable.rows[i].deleteCell(x);
         }
         let cells = this.keyTable.rows[i].insertCell(x);
         cells.textContent = "-";
@@ -45,68 +47,46 @@ let key = {
     
   },
   keyListener: function(action) {
-    document.addEventListener(action, event => {      
+    document.addEventListener(action, event => {
       // prevent TAB key from defaulting
       if (event.which === 9 || event.keyCode === 9) { 
           event.preventDefault();
       }
-      
+      // key codes - a number which represents an actual key on the keyboard
       this.keyEvent[action] = event.which || event.keyCode;
+      // character code - a number that represents the unicode character of the key
       this.charEvent[action] = event.charCode;
-      
       
       if (action === "keydown") {
         this.clearCell();
         this.toggleDisplay();
-        
-        const cellPos = 1;
-        this.updateCell(this.keycode, this.keyEvent["keydown"], cellPos);
-        this.updateCell(this.charcode, this.charEvent["keydown"], cellPos);
-        this.updateCell(this.character, event.key, cellPos);
-        this.updateCell(this.shift, event.shiftKey, cellPos);
-        this.updateCell(this.ctrl, event.ctrlKey, cellPos);
-        this.updateCell(this.alt, event.altKey, cellPos);
-        this.updateCell(this.meta, event.metaKey, cellPos);          
-        
+        this.render(action, 1);
       } else if (action === "keypress") {
-        const cellPos = 2;
-        
-        this.updateCell(this.keycode, this.keyEvent["keypress"], cellPos);
-        this.updateCell(this.charcode, this.charEvent["keypress"], cellPos);
-        
-        const keypressCell = this.keycode.cells[cellPos].textContent;
-        const char = String.fromCharCode(keypressCell);
-        this.updateCell(this.character, char, cellPos);
-        
-        this.updateCell(this.shift, event.shiftKey, cellPos);
-        this.updateCell(this.ctrl, event.ctrlKey, cellPos);
-        this.updateCell(this.alt, event.altKey, cellPos);
-        this.updateCell(this.meta, event.metaKey, cellPos);
-        
+        this.render(action, 2);
       } else if (action === "keyup") {
-        const cellPos = 3;
-        
-        this.updateCell(this.keycode, this.keyEvent["keyup"], cellPos);
-        this.updateCell(this.charcode, this.charEvent["keyup"], cellPos);
-        this.updateCell(this.character, event.key, cellPos);
-        this.updateCell(this.shift, event.shiftKey, cellPos);
-        this.updateCell(this.ctrl, event.ctrlKey, cellPos);
-        this.updateCell(this.alt, event.altKey, cellPos);
-        this.updateCell(this.meta, event.metaKey, cellPos);
-        
+        this.render(action, 3);
       }
       
     });
     
   },
   bindEvents: function() {
-    // hides table
-    this.toggle[1].style.display = "none";
-    
     this.keyListener("keydown");
     this.keyListener("keypress");
     this.keyListener("keyup");
-
+  },
+  render: function(action, cellPos) {
+    this.updateCell(this.keycode, this.keyEvent[action], cellPos);
+    this.updateCell(this.charcode, this.charEvent[action], cellPos);
+    this.updateCell(this.character, event.key, cellPos);
+    this.updateCell(this.shift, event.shiftKey, cellPos);
+    this.updateCell(this.ctrl, event.ctrlKey, cellPos);
+    this.updateCell(this.alt, event.altKey, cellPos);
+    this.updateCell(this.meta, event.metaKey, cellPos);
+    
+  },
+  startPrompt: function () {
+    this.toggle[0].style.display = "block";
   },
   toggleDisplay: function() {
     let heading = this.toggle[0].style;
